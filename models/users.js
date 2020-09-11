cbc54c498bc7c83d385b623a374e8ba4ce704e6e
rohitthapa2320 <rohitthapa2362000@gmail.com>
2020-09-11 21:23:56 +0530

    Passport

--- models/users.js
+++ models/users.js
@@ -1,21 +1,16 @@
 const mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+   
     admin:{
         type: Boolean,
         default: false
     }
 
 });
+
+User.plugin(passportLocalMongoose)
 
 module.exports = mongoose.model('User', User);
