c2cf23882618cd81957cd96958e118e63e97da31
rohitthapa2320 <rohitthapa2362000@gmail.com>
2020-10-04 20:33:16 +0530

    Favorites Router

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var dishRouter= require('./routes/dishRouter');
 var promoRouter= require('./routes/promoRouter');
 var leaderRouter= require('./routes/leaderRouter');
 var uploadRouter= require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 
 const Dishes= require('./models/dishes');
@@ -63,6 +64,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 
 // catch 404 and forward to error handler
